7820a77a5692426c1046171de4d40d3040ed8fcd
Salama <ma7moud.salamaaa@gmail.com>
2025-10-22 15:04:54 +0300

    ddsdds

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -206,24 +206,24 @@ Assigned to: ThemeForest
 	},
 	/*------------------------------------------------------------------*/
 	// Start testimonial swipper slider
-	testimonial_slider: function() {
-		if($('.port_testimonial_setions .swiper-container').length > 0){
-			var swiper = new Swiper('.port_testimonial_setions .swiper-container', {
-			  slidesPerView: 1,
-			  spaceBetween: 30,
-			  loop: true,
-			  speed:1000,
-			   navigation: {
-				nextEl: '.swiper-button-next',
-				prevEl: '.swiper-button-prev',
-				},
-			  autoplay: {
-					delay: 5500,
-					disableOnInteraction: false,
-				},
-			});
-		}
-	},
+	//testimonial_slider: function() {
+	//	if($('.port_testimonial_setions .swiper-container').length > 0){
+	//		var swiper = new Swiper('.port_testimonial_setions .swiper-container', {
+	//		  slidesPerView: 1,
+	//		  spaceBetween: 30,
+	//		  loop: true,
+	//		  speed:1000,
+	//		   navigation: {
+	//			nextEl: '.swiper-button-next',
+	//			prevEl: '.swiper-button-prev',
+	//			},
+	//		  autoplay: {
+	//				delay: 5500,
+	//				disableOnInteraction: false,
+	//			},
+	//		});
+	//	}
+	//},
 	// End testimonial swipper slider
 	/*------------------------------------------------------------------*/ 
 	
